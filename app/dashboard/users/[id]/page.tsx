6045129d5df2cd7f176a1ef246506b6e149db6c8
Ratul-9 <mukherjeearatul2023@gmail.com>
2025-01-03 00:12:14 +0530

    Second Commit of learning

---
+++ app/dashboard/users/[id]/page.tsx
@@ -0,0 +1,6 @@
+async function Page({params}:{params: {id: string}}) {
+    const { id } = await params;
+    return (
+        <h1 className="text-3xl">User Profile: {id}</h1>
+    );
+}export default Page;
